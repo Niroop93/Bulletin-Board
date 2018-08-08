fc80fee8745f11718b0419678e9993cecf8b0c1c
Niroop <niroop093@gmail.com>
2018-08-08 17:55:21 -0400

    added board and implemented update feature

--- src/Note.js
+++ src/Note.js
@@ -26,16 +26,20 @@ class Note extends Component {
 		alert('removing note')
 	}
 
-	save() {
-		alert(this._newText.value)
+	save(e) {
+		e.preventDefault()
+		this.props.onChange(this._newText.value, this.props.index)
+		this.setState({
+			editing: false
+		})
 	}
 
 	renderForm() {
 		return (
 			<div className = "note">
-				<form>
+				<form onSubmit={this.save} >
 					<textarea ref={ input => this._newText = input} />
-					<button onClick= {this.save}> <FaSave /> </button>
+					<button id="save"> <FaSave /> </button>
 				</form>
 			</div>
 			)
@@ -44,7 +48,7 @@ class Note extends Component {
 	renderDisplay() {
 		return (
 				<div className = "note">
-					<p> Learn React </p>
+					<p> {this.props.children} </p>
 					<span>
 						<button onClick={this.edit} id= "edit" > < FaPencilAlt /> </button>
 						<button onClick={this.remove} id= "remove"> < FaTrash /> </button>
